aff073c05868f9f26899057ed374ea401e528936
vickonrails <victorofoegbu0009@gmail.com>
2021-12-16 10:55:04 +0100

    refactor: use env grapqhl endpoint

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -9,11 +9,10 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { GlobalStyles } from "../components";
 import theme from "../theme";
 
-import client from "../apollo-client";
-
+const graphqlEndpoint = process.env.GRAPHQL_ENDPOINT as string;
 // setup react-query client
 const queryClient = new QueryClient();
-export const graphqlClient = new GraphQLClient("http://localhost:5000", {
+export const graphqlClient = new GraphQLClient(graphqlEndpoint, {
   headers: {
     Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NjE3YTI2YS1iMTBlLTRhMDktYWM2Zi0wYzRkYmIwNmU5ODIiLCJlbWFpbCI6InZpY3Rvcm9vZWd1MDAwOUBnbWFpbC5jb20iLCJpYXQiOjE2Mzk1NDcyMjcsImV4cCI6MTY0MjEzOTIyN30.uEYlykdap0iXBNFmT7xWx3FTUOJvkgtLSlO5xD48hy8`,
   },
