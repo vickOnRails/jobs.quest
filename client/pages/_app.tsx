167213c62804c67f464658cc46391f0ef124a5d9
vickonrails <victorofoegbu0009@gmail.com>
2021-12-16 18:25:11 +0100

    fix: use appropriate browser env variables

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -8,12 +8,12 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { GlobalStyles } from "../components";
 import theme from "../theme";
 
-const graphqlEndpoint = process.env.GRAPHQL_ENDPOINT as string;
+const graphqlEndpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string;
 // setup react-query client
 const queryClient = new QueryClient();
 export const graphqlClient = new GraphQLClient(graphqlEndpoint, {
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NjE3YTI2YS1iMTBlLTRhMDktYWM2Zi0wYzRkYmIwNmU5ODIiLCJlbWFpbCI6InZpY3Rvcm9vZWd1MDAwOUBnbWFpbC5jb20iLCJpYXQiOjE2Mzk1NDcyMjcsImV4cCI6MTY0MjEzOTIyN30.uEYlykdap0iXBNFmT7xWx3FTUOJvkgtLSlO5xD48hy8`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
   },
 });
 
